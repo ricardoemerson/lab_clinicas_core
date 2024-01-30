7e1c24157066d6a6fe02e9f9922ab71cd14418f1
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2024-01-30 13:10:31 -0300

    feat: setup rest client with auth interceptor and app loader

---
+++ lib/src/lab_clinicas_core_config.dart
@@ -0,0 +1,44 @@
+import 'package:asyncstate/asyncstate.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+import 'package:lab_clinicas_core/lab_clinicas_core.dart';
+
+class LabClinicasCoreConfig extends StatelessWidget {
+  final ApplicationBindings? bindings;
+  final List<FlutterGetItPageRouter>? pages;
+  final List<FlutterGetItPageBuilder>? pagesBuilders;
+  final List<FlutterGetItModule>? modules;
+  final String title;
+
+  const LabClinicasCoreConfig({
+    Key? key,
+    this.bindings,
+    this.pages,
+    this.pagesBuilders,
+    this.modules,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterGetIt(
+      debugMode: kDebugMode,
+      bindings: bindings,
+      pages: [...pages ?? [], ...pagesBuilders ?? []],
+      modules: modules,
+      builder: (context, routes, flutterGetItNavObserver) {
+        return AsyncStateBuilder(
+          loader: LabClinicasLoader(),
+          builder: (navigatorObserver) {
+            return MaterialApp(
+              title: title,
+              navigatorObservers: [navigatorObserver, flutterGetItNavObserver],
+              routes: routes,
+            );
+          },
+        );
+      },
+    );
+  }
+}
