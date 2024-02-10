2c9e944829d338538ccb87dd23dae38c9a770f94
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2024-02-10 16:20:07 -0300

    feat: add support for Dio for web

--- lib/lab_clinicas_core.dart
+++ lib/lab_clinicas_core.dart
@@ -12,7 +12,7 @@ export 'src/fp/unit.dart';
 export 'src/helpers/messages.dart';
 export 'src/lab_clinicas_core_config.dart';
 export 'src/loader/lab_clinicas_loader.dart';
-export 'src/rest_client/rest_client.dart';
+export 'src/rest_client/rest_client.dart' if (dart.library.html) 'src/rest_client/rest_client_web.dart';
 export 'src/theme/app_theme.dart';
 export 'src/widgets/app_loader.dart';
 export 'src/widgets/icon_popup_menu_button.dart';
